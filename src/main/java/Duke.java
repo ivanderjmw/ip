95965756c2fd0f94eb8a05003de93a21c2806e01
Ivander <ivanderjmw@gmail.com>
2020-08-21 16:26:37 +0800

    Add Greeting
    Level-0 increment. Create a skeletal version that simply greets the user and exists.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,22 @@
 public class Duke {
     public static void main(String[] args) {
+        String line = "____________________________________________________________";
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+        String welcomeMessage = "Hello! I'm Duke\n" +
+                "What can I do for you?";
+        String byeMessage = "Bye. Hope to see you again soon!";
         System.out.println("Hello from\n" + logo);
+
+        System.out.println(line);
+        System.out.println(welcomeMessage);
+        System.out.println(line);
+
+        System.out.println(byeMessage);
+        System.out.println(line);
+
     }
 }
